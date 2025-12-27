6afe5c31918f2701bf40ba284c297e889f9d2bef
amulKumarverma <amulkumar316@email.com>
2025-12-27 20:20:17 +0530

    modified:   Pattern/pattern16.exe 	modified:   Pattern/pattern20.exe 	modified:   Pattern/pattern28.cpp 	modified:   Pattern/pattern28.exe 	modified:   Pattern/pattern3.exe

--- Pattern/pattern28.cpp
+++ Pattern/pattern28.cpp
@@ -1,8 +1,53 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 int main()
 {
 
+    int n;
+    cout << "Enter the number: ";
+    cin >> n;
+
+    for (int i = n; i >= 1; i--)
+    {
+
+        for (int l = 1; l <= i; l++)
+        {
+            cout << "* ";
+        }
+
+        for (int l = 1; l <= 2 * n - 2 * i; l++)
+        {
+            cout << "  ";
+        }
+
+        for (int l = 1; l <= i; l++)
+        {
+            cout << "* ";
+        }
+
+        cout << endl;
+    }
+
+    for (int i = 1; i <= n; i++)
+    {
+
+        for (int l = 1; l <= i; l++)
+        {
+            cout << "* ";
+        }
+
+        for (int l = 1; l <= 2 * n - 2 * i; l++)
+        {
+            cout << "  ";
+        }
+
+        for (int l = 1; l <= i; l++)
+        {
+            cout << "* ";
+        }
+
+        cout << endl;
+    }
 
     return 0;
 }
---
+++ Pattern/tempCodeRunnerFile.cpp
@@ -0,0 +1,3 @@
+    for(int l =n; l>=i;l--){
+cout<<"* ";
+}
